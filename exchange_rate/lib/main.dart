6814066330c7af62fab782fed41807be677d8a7a
KIWOLY <ikiwoly@gmail.com>
2026-01-12 13:32:52 +0300

    add splash screen

--- exchange_rate/lib/main.dart
+++ exchange_rate/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:exchange_rate/pages/currency_convert.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
